28e9b5a00f3581309595c2acb94fb860afd14aaf
trolland <trolland@student.42.fr>
2024-05-29 15:36:05 +0200

    Auto-updating from 42

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: trolland <trolland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 17:38:07 by trolland          #+#    #+#             */
-/*   Updated: 2024/05/28 19:49:10 by trolland         ###   ########.fr       */
+/*   Updated: 2024/05/29 15:28:56 by trolland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 
 typedef struct s_char
 {
-	int				sig_no;
+	char			sig_no;
 	char			c;
 	int				pid;
 	struct s_char	*next;
@@ -31,10 +31,6 @@ typedef struct s_char
 
 }					t_char;
 
-// t_char				*ft_new_node(char c);
-// void				ft_add_back(t_char **lst, t_char *new);
-// void				free_string(t_char *string);
-// void				ft_print_string(t_char **string);
 
 t_char				*ft_new_node(char c, int pid);
 void				ft_add_back(t_char **lst, t_char *new);
